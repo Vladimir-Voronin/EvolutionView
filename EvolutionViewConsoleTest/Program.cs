6ba9d0066c8b15790044783bad8896ccb826b064
Воронин Владимир <vladimirvoron7@gmail.com>
2021-12-15 15:52:25 +0300

    Add project files.

---
+++ EvolutionViewConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvolutionViewConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
